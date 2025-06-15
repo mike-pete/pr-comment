183f23f35d6bc5ec03995ac0d659c047db64f828
Mike Peterson <mike@lemonshell.com>
2025-06-14 18:04:48 -0700

    test: add PR comments for demo PR comment bot

--- test-pr-comment.ts
+++ test-pr-comment.ts
@@ -1,8 +1,11 @@
-
+// PR: This is a single-line PR comment
 
 const foo = 42
 
+/* PR: This is a multi-line
+PR comment example */
 
+// PR: Another test PR comment for the demo
 
 function bar() {
 	// Not a PR comment
