4c09a004521bb86781fbb1b4634f883d4e7b0fe7
Mike Peterson <mike@lemonshell.com>
2025-06-14 18:36:04 -0700

    test: add new PR comment for fresh workflow test

--- test-pr-comment.ts
+++ test-pr-comment.ts
@@ -7,6 +7,8 @@ PR comment example */
 
 // PR: Another test PR comment for the demo
 
+// PR: This is a new PR comment for a fresh workflow test
+
 function bar() {
 	// Not a PR comment
 	return foo
