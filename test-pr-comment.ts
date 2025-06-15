cc5f2cd7e93a5620493108c7ac5cd6d16b692686
Mike Peterson <mike@lemonshell.com>
2025-06-14 19:50:19 -0700

    wip

--- test-pr-comment.ts
+++ test-pr-comment.ts
@@ -1,13 +1,6 @@
-// PR: This is a single-line PR comment
 
 const foo = 42
-
-/* PR: This is a multi-line
-PR comment example */
-
-// PR: Another test PR comment for the demo
-
-// PR: This is a new PR comment for a fresh workflow test
+// PR: huh
 
 function bar() {
 	// Not a PR comment
